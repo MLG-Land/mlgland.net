85d7e4172bbea5d99ff1d26c849250b35c8d97c1
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-28 17:03:24 -0400

    spread out code

---
+++ updates.js
@@ -0,0 +1,15 @@
+var xhttp = new XMLHttpRequest();
+						xhttp.onreadystatechange = function () {
+							if (this.readyState == 4 && this.status == 200) {
+								//we will make a variable to hold the text
+								var text = this.responseText;
+								//now we make it so that each line will be seperated by a <br>
+								text = text.replace(/\n/g, "<br>");
+								//now we will display the text
+								document.getElementById(
+									"latest-updates"
+								).innerHTML = text;
+							}
+						};
+						xhttp.open("GET", "./updates.txt", true);
+						xhttp.send();
