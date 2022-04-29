5d91ac2662b5f2b61695631d971043a73765754f
Sid <saytleader@outlook.com>
2022-04-29 13:09:27 -0400

    Update index.php

--- games/index.php
+++ games/index.php
@@ -2,13 +2,14 @@
 <html lang="en">
 
 <head>
-  <meta name="description" content="Webpage description goes here" />
-  <meta charset="utf-8">
-  <title>MLG Land Games</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="">
-  <link rel="stylesheet" href="../style.css">
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  	<meta name="description" content="Webpage description goes here" />
+  	<meta charset="utf-8">
+  	<title>MLG Land Games</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<meta name="author" content="">
+  	<link rel="stylesheet" href="../style.css">
+	<link rel="stylesheet" href="card.css">
+  	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 
 <body>
