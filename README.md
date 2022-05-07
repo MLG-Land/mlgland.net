fa122aa369e6b67a6be0f11c5f24f37aaaa8e6c4
Nathan Chu <63111210+nathanchu@users.noreply.github.com>
2022-05-06 21:38:00 -0400

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# mlgland.ddns.net
---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("scroll_down_it_wont_hurt_you").addEventListener("click", () => {
+    window.scrollBy(0, 160)
+})
+
+axios.get('alerts.html').then((res) => {
+    document.getElementById("alerts_body").innerHTML = res.data
+})
+
+axios.get('updates.html').then((res) => {
+    document.getElementById("updates_body").innerHTML = res.data
+})
