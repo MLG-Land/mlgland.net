069805ff770121770022c0826c87f76f8fa5675e
Sid <saytleader@outlook.com>
2022-05-10 11:00:06 -0400

    Create index.php

---
+++ page/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+    switch ($_GET["profile"]) {
+			case "SuperBoyThomas6":
+				echo "<meta name="description" content="SuperBoyThomas6\' profile" />";
+				echo "<title>SuperBoyThomas6\'s profile</title>";
+				break;
+		}
+  ?>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="../style.css">
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+</head>
+
+<body>
+  
+<div class="container">
+  
+</div>
+
+<script>
+</script>
+
+</body>
+</html>
