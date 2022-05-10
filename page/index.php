e9ed3962d7cd6501216ac4e495b152c91f28bf36
Sid <saytleader@outlook.com>
2022-05-10 13:10:19 -0400

    Update index.php

--- page/index.php
+++ page/index.php
@@ -14,16 +14,11 @@
 				break;
 		}
   ?>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="">
-  <link rel="stylesheet" href="../style.css">
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<?php
-	if (empty($_GET["profile"])) {
-		echo "<script>window.location.href = error</script>";
-	}
-	?>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<meta name="author" content="">
+  	<link rel="stylesheet" href="../style.css">
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 
 <body>
