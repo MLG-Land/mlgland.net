02d5eae5966a5ef527d6a125fb7097bcd0f8bbc7
Sid <saytleader@outlook.com>
2022-05-13 17:54:39 -0400

    finally format the document

--- serverstat.js
+++ serverstat.js
@@ -1,27 +1,27 @@
 MinecraftAPI.getServerStatus(
-					"mc.mlgland.net",
-					{
-						port: 25565, // optional, only if you need a custom port
-					},
-					function (err, status) {
-						if (err) {
-							return (document.querySelector(
-								".status"
-							).innerHTML = "Error loading status");
-						}
+	"mc.mlgland.net",
+	{
+		port: 25565, // optional, only if you need a custom port
+	},
+	function (err, status) {
+		if (err) {
+			return (document.querySelector(
+				".status"
+			).innerHTML = "Error loading status");
+		}
 
-						// you can change these to your own message!
-						document.querySelector(".upstate").innerHTML =
-							status.online
-								? "Status: Online"
-								: "Status: Offline";
-						document.querySelector(".players").innerHTML =
-							"Players: " +
-							status.players.now +
-							"/" +
-							status.players.max +
-							"";
-						document.querySelector(".version").innerHTML =
-							"Version: " + status.server.name;
-					}
-					);
+		// you can change these to your own message!
+		document.querySelector(".upstate").innerHTML =
+			status.online
+				? "Status: Online"
+				: "Status: Offline";
+		document.querySelector(".players").innerHTML =
+			"Players: " +
+			status.players.now +
+			"/" +
+			status.players.max +
+			"";
+		document.querySelector(".version").innerHTML =
+			"Version: " + status.server.name;
+	}
+);
