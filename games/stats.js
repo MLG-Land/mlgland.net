cd7c5a146eaeb665d7a24d2efa9c8ed6cab78c58
Sid <saytleader@outlook.com>
2022-05-16 13:10:38 -0400

    Update stats.js

--- games/stats.js
+++ games/stats.js
@@ -7,3 +7,34 @@
 cards made so far:
 - skywars
 */
+
+var skywars_cur;
+var skyward_max;
+
+MinecraftAPI.getServerStatus(
+	"mc.mlgland.net",
+	{
+		port: 10000, // optional, only if you need a custom port
+	},
+	function (err, status) {
+		if (err) {
+			return (document.querySelector(
+				".status"
+			).innerHTML = "Error loading status");
+		}
+
+		// you can change these to your own message!
+		document.querySelector(".upstate").innerHTML =
+			status.online
+				? "Status: Online"
+				: "Status: Offline";
+		document.querySelector(".players").innerHTML =
+			"Players: " +
+			status.players.now +
+			"/" +
+			status.players.max +
+			"";
+		document.querySelector(".version").innerHTML =
+			"Version: " + status.server.name;
+	}
+);
