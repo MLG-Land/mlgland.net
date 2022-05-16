d30eefe229a45def09273c7a2448153cf38eb1f3
Sid <saytleader@outlook.com>
2022-05-16 13:06:10 -0400

    Create stats.js

---
+++ games/stats.js
@@ -0,0 +1,9 @@
+// <game>-player-stat
+
+// we need to use the minecraft server api to find the amount of players on each server
+// just a reminder for one of the sysadmins: forwars all the query ports for each server
+
+/*
+cards made so far:
+- skywars
+*/
