65a8423c9f7b0b55d47158b20af36a4d966edc62
Sid <saytleader@outlook.com>
2022-05-18 10:09:38 -0400

    Create backgrounds.js

---
+++ games/backgrounds.js
@@ -0,0 +1,16 @@
+function change(id) {
+	switch (id) {
+		case 0:
+			// set the background to default
+			break;
+		case 1:
+			// set the background to a picture of skywars
+			break;
+		case 2:
+			// set the background to a picture of bedwars
+			break;
+		case 3:
+			// set the background to a picture of fastbuilder
+			break;
+	}
+}
