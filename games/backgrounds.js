2025604c1dce223248decd669569f7d38adf01c5
Sid <saytleader@outlook.com>
2022-05-18 10:19:55 -0400

    Update backgrounds.js

--- games/backgrounds.js
+++ games/backgrounds.js
@@ -2,15 +2,19 @@ function change(id) {
 	switch (id) {
 		case 0:
 			// set the background to default
+			document.getElementById("background-container").style.backgroundImage: "url('../image.jpg')";
 			break;
 		case 1:
 			// set the background to a picture of skywars
+			document.getElementById("background-container").style.backgroundImage: "url('../assets/skywars-map.jpg')";
 			break;
 		case 2:
 			// set the background to a picture of bedwars
+			document.getElementById("background-container").style.backgroundImage: "url('../assets/bedwars-map.jpg')";
 			break;
 		case 3:
 			// set the background to a picture of fastbuilder
+			document.getElementById("background-container").style.backgroundImage: "url('../assets/fastbuilder-map.jpg')";
 			break;
 	}
 }
