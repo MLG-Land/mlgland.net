fe77b854b89b3cbc7f6842485f3895a33d694f52
Sid <saytleader@outlook.com>
2022-05-20 14:17:44 -0400

    Update main.js

--- jstest/main.js
+++ jstest/main.js
@@ -1,5 +1,4 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-var text = url.searchParams.get("text");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
-document.getElementById("text").innerHTML = text;
+document.getElementById("text").innerHTML = urlParams.get('text');
